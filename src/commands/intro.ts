8a7def716e3edbe2203e963ba78e263828a93da2
Duncan Leo <hello@duncanleo.me>
2022-05-03 12:31:10 +0800

    feat: add sample /intro command

---
+++ src/commands/intro.ts
@@ -0,0 +1,48 @@
+import Command from '../Command';
+
+export default class IntroCommand extends Command {
+  private _name: string | null = null;
+  private _age: number | null = null;
+
+  get name(): string {
+    return 'intro';
+  }
+
+  get stages(): App.CommandStage<this>[] {
+    return [
+      {
+        trigger: {
+          type: 'command',
+          command: 'intro',
+        },
+        async handle(msg) {
+          await this.bot.sendMessage(msg.chat.id, 'What is your name?');
+        },
+      },
+      {
+        trigger: {
+          type: 'text',
+        },
+        async handle(msg) {
+          this._name = msg.text ?? '';
+
+          await this.bot.sendMessage(msg.chat.id, 'I see, what is your age?');
+        },
+      },
+      {
+        trigger: {
+          type: 'text',
+          matcher: /\d+/,
+        },
+        async handle(msg) {
+          this._age = parseInt(msg.text ?? '0');
+
+          await this.bot.sendMessage(
+            msg.chat.id,
+            `Hi ${this._name} of age ${this._age}!`
+          );
+        },
+      },
+    ];
+  }
+}
